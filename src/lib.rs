0460d29d93ea575b62fa34fb6036d784f97725a6
David Meyer <david@nscope.org>
2020-05-02 12:06:11 -0500

    use git-version crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
 mod lab_bench;
 mod nscope;
-
-mod built_info {
-    include!(concat!(env!("OUT_DIR"), "/built.rs"));
-}
+use git_version::git_version;
 
 pub use lab_bench::LabBench;
 
@@ -17,12 +14,8 @@ pub fn ver() -> String {
     let mut build = String::from(env!("CARGO_PKG_VERSION"));
     build += "+";
 
-    if built_info::GIT_VERSION.is_some() {
-        build.push_str(built_info::GIT_VERSION.unwrap_or(""));
-    }
-    if built_info::CI_PLATFORM.is_some() {
-        build.push_str(built_info::CI_PLATFORM.unwrap_or(""));
-    }
+    const GIT_VERSION: &str = git_version!();
+    build.push_str(GIT_VERSION);
     build
 }
 
@@ -34,7 +27,7 @@ mod tests {
     #[test]
     fn version_is_valid_semver() {
         let v = ver();
-        assert!(Version::parse(&v).is_ok());
         println!("v{}", v);
+        assert!(Version::parse(&v).is_ok());
     }
 }
