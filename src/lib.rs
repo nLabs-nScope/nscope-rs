4394a43cfabfcd07096a3dd97215fc9a90b787b4
David Meyer <david@nscope.org>
2020-05-02 15:46:34 -0500

    add test to ensure cargo version matches git version

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 mod lab_bench;
 mod nscope;
+
 use git_version::git_version;
+use semver::Version;
 
 pub use lab_bench::LabBench;
 
@@ -10,24 +12,58 @@ pub enum NscopeError {
     UnknownError { message: String },
 }
 
-pub fn ver() -> String {
-    let mut build = String::from(env!("CARGO_PKG_VERSION"));
-    build += "+";
+pub fn ver() -> Result<String, String> {
+    let cargo_version = env!("CARGO_PKG_VERSION").to_owned();
+    let git_description = git_version!(args = ["--always", "--tags", "--dirty"]);
 
-    const GIT_VERSION: &str = git_version!();
-    build.push_str(GIT_VERSION);
-    build
+    let build_str = match Version::parse(git_description) {
+        Ok(git_semver) => format!("{}", git_semver.pre[0].to_string()),
+        Err(_) => git_description.to_string(),
+    };
+    Ok(format!("{}+{}", cargo_version, build_str))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use semver::Version;
+
+    #[test]
+    fn cargo_version_matches_git_tag() {
+        let cargo_version = env!("CARGO_PKG_VERSION").to_owned();
+        let git_description = git_version!(args = ["--always", "--tags", "--dirty"]);
+
+        let cargo_semver = Version::parse(&cargo_version);
+        let git_semver = Version::parse(&git_description);
+
+        assert!(
+            cargo_semver.is_ok(),
+            "invalid semver cargo version: {}",
+            cargo_version
+        );
+        assert!(
+            git_semver.is_ok(),
+            "invalid semver in git description: {}",
+            git_description
+        );
+        let cargo_semver = cargo_semver.unwrap();
+        let git_semver = git_semver.unwrap();
+
+        assert!(
+            cargo_semver.major == git_semver.major
+                && cargo_semver.minor == git_semver.minor
+                && cargo_semver.patch == git_semver.patch,
+            "cargo version {} does not match git description {}",
+            cargo_version,
+            git_description
+        );
+    }
 
     #[test]
     fn version_is_valid_semver() {
         let v = ver();
-        println!("v{}", v);
-        assert!(Version::parse(&v).is_ok());
+        assert!(v.is_ok(), "invalid version: {}", v.unwrap());
+        let v = v.unwrap();
+        assert!(Version::parse(&v).is_ok(), "invalid version: {}", v);
+        println!("v{}", ver().unwrap())
     }
 }
