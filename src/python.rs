66fef580165db63b0ee7b3bf9641cba0cd89986e
David Meyer <10246856+davidjmeyer@users.noreply.github.com>
2024-11-07 20:46:36 -0800

    add nlab cli to the python package (#21)
    * add nlab cli to the python package
    * add in some print statements
    * allow calling update from python code

--- src/python.rs
+++ src/python.rs
@@ -2,15 +2,28 @@ mod bench;
 mod scope;
 mod analog_output;
 mod pulse_output;
+mod cli;
 
 use pyo3::prelude::*;
 use crate::{AnalogSignalPolarity, AnalogWaveType, PowerStatus, PowerState};
+use cli::{Cli, Commands};
+use clap::Parser;
 
 #[pyclass]
 struct LabBench;
 
 #[pyclass]
 struct Nlab(crate::Nlab);
+
+#[pyfunction]
+fn run_cli(_py: Python) -> PyResult<()> {
+    let args: Vec<_> = std::env::args_os().skip(1).collect();
+    let cli = Cli::parse_from(args);
+
+    match &cli.command {
+        Commands::Update => { LabBench::update_all_nlabs() }
+    }
+}
 
 /// A Python module implemented in Rust.
 #[pymodule]
@@ -21,5 +34,6 @@ fn nlabapi(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<AnalogSignalPolarity>()?;
     m.add_class::<PowerState>()?;
     m.add_class::<PowerStatus>()?;
+    m.add_function(wrap_pyfunction!(run_cli, m)?)?;
     Ok(())
 }
